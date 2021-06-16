6e0a18219272a6f1d5bbf34873e2141894bbceac
Kaveer Kapoor <kaveerkapoor93@gmail.com>
2021-06-15 18:28:48 -0700

    chore: add Shopify Layout, Page and EmptyState

--- pages/_app.js
+++ pages/_app.js
@@ -2,26 +2,22 @@ import React from "react";
 import App from "next/app";
 import Head from "next/head";
 import { AppProvider } from "@shopify/polaris";
-import { Provider } from "@shopify/app-bridge-react";
 import "@shopify/polaris/dist/styles.css";
 import translations from "@shopify/polaris/locales/en.json";
 
 class StreamrAdminApp extends App {
   render() {
-    const { Component, pageProps, shopOrigin } = this.props;
+    const { Component, pageProps } = this.props;
 
-    const config = { apiKey: API_KEY, shopOrigin, forceRedirect: true };
     return (
       <React.Fragment>
         <Head>
           <title>Streamr Admin</title>
           <meta charSet="utf-8" />
         </Head>
-        <Provider config={config}>
-          <AppProvider i18n={translations}>
-            <Component {...pageProps} />
-          </AppProvider>
-        </Provider>
+        <AppProvider i18n={translations}>
+          <Component {...pageProps} />
+        </AppProvider>
       </React.Fragment>
     );
   }
